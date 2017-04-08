8cf859bf0bf2779348be1a4f178bf76c35ae462a
Keijiro Takahashi <keijiro@X1.local>
2017-04-08 17:17:23 +0900

    Added OSX and Linux support.

--- Assets/FFmpegOut/FFmpegConfig.cs
+++ Assets/FFmpegOut/FFmpegConfig.cs
@@ -4,13 +4,25 @@ namespace FFmpegOut
 {
     static class FFmpegConfig
     {
-        public static string BinaryPath {
+        public static string BinaryPath
+        {
             get {
-                return Application.streamingAssetsPath + "/FFmpegOut/ffmpeg.exe";
+                var basePath = Application.streamingAssetsPath + "/FFmpegOut";
+                
+                if (Application.platform == RuntimePlatform.OSXPlayer ||
+                    Application.platform == RuntimePlatform.OSXEditor)
+                    return basePath + "/OSX/ffmpeg";
+
+                if (Application.platform == RuntimePlatform.LinuxPlayer ||
+                    Application.platform == RuntimePlatform.LinuxEditor)
+                    return basePath + "/Linux/ffmpeg";
+
+                return basePath + "/Windows/ffmpeg.exe";
             }
         }
 
-        public static bool CheckAvailable {
+        public static bool CheckAvailable
+        {
             get { return System.IO.File.Exists(BinaryPath); }
         }
     }
