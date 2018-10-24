df4a31fed3d40c301afc588326a46eb4d7d67f2e
Keijiro Takahashi <keijiro@gmail.com>
2018-10-24 21:44:40 +0900

    Add FrameRateController

---
+++ Assets/FFmpegOut/Runtime/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+// FFmpegOut - FFmpeg video encoding plugin for Unity
+// https://github.com/keijiro/KlakNDI
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("FFmpegOut.Editor")]
