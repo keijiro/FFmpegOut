6e412f4595289daca53abbac8a40f09d6c0e3808
stefan <52441940+stef-t@users.noreply.github.com>
2019-07-18 11:34:07 +0200

    deriving from FFmpegOut classes to create everything required to capture a Camera LiveStream; setting up ffmpeg argument presets for different types of live stream protocols

---
+++ Assets/FFmpegOut/Runtime/LiveStream/StreamCameraCapture.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace FFmpegOut.LiveStream
+{
+    public class StreamCameraCapture : CameraCapture
+    {
+        [SerializeField] protected string _streamAddress;
+        [SerializeField] protected StreamPreset _streamPreset;
+
+        protected override FFmpegSession GetSession(int texWidth, int texHeight)
+        {
+            return StreamFFmpegSession.Create(
+                texWidth,
+                texHeight,
+                frameRate,
+                preset,
+                _streamPreset,
+                _streamAddress);
+        }
+    }
+}
---
+++ Assets/FFmpegOut/Runtime/LiveStream/StreamPreset.cs
@@ -0,0 +1,42 @@
+﻿namespace FFmpegOut.LiveStream
+{
+    public enum StreamPreset
+    {
+        Udp,
+        Rtp,
+        Rtsp,
+        Hls,
+        Hls_ssegment,
+        Rtmp
+    }
+
+    public static class StreamPresetExtensions
+    {
+        public static string GetOptions(this StreamPreset preset)
+        {
+            switch (preset)
+            {
+                case StreamPreset.Udp:
+                    //Inspector: [udp adress] / udp://192.168.0.1:10755
+                    return "-f mpegts";
+                case StreamPreset.Rtp:
+                    //Inspector: [rtp adress] / rtp://192.168.0.1:10755
+                    return "-f rtp_mpegts";
+                case StreamPreset.Rtsp:
+                    //Inspector: [rtsp adress] / rtsp://192.168.0.1:10755
+                    return "-f rtsp";
+                case StreamPreset.Hls:
+                    //Inspector: [http server base url] [htdocs directory path + m3u8 file name extension] / http://192.168.0.1:8000/ D:\Repo\UnityCameraStream\miniweb\htdocs\stream.m3u8
+                    return "-f hls -hls_flags delete_segments -hls_init_time 0.5 -hls_time 0.5 -hls_list_size 10 -hls_allow_cache 1 -hls_base_url";
+                case StreamPreset.Hls_ssegment:
+                    //Inspector: [http server base url] -segment_list [htdocs directory path + m3u8 file name extension] [htdocs directory path + ts file name extension] / http://192.168.0.185:8000/ -segment_list D:\Repo\UnityCameraStream\miniweb\htdocs\stream.m3u8 D:\Repo\UnityCameraStream\miniweb\htdocs\out%03d.ts
+                    return "-f segment -segment_list_type m3u8 -segment_list_size 10 -segment_list_flags +live -segment_time 1 -segment_wrap 10 -segment_list_entry_prefix";
+                case StreamPreset.Rtmp:
+                    //Inspector: [rtmp adress] / rtmp://192.168.0.1:1935/rtmp_stream/mystream
+                    return "-f flv";
+            }
+
+            return null;
+        }
+    }
+}
